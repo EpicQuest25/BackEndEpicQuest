83fb6c980f88b71da3b98f88590ab8a227e4bab9
Epic Quest <epicquest25@gmail.com>
2025-07-26 05:34:56 +0800

    Fix dependency injection in PaymentGatewayModule

--- src/payment-gateway/payment-gateway.module.ts
+++ src/payment-gateway/payment-gateway.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentGatewayController } from './payment-gateway.controller';
 import { PaymentGatewayService } from './payment-gateway.service';
+import { Transection } from '../transection/entities/transection.entity';
+import { User } from '../user/entities/user.entity';
+import { Agent } from '../agent/entities/agent.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([Transection, User, Agent]),
+  ],
   controllers: [PaymentGatewayController],
   providers: [PaymentGatewayService],
   exports: [PaymentGatewayService],
